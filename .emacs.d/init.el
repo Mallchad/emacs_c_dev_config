10e426b866355893179945f644c2b09fc4d1e020
Mallchad <45771609+Mallchad@users.noreply.github.com>
2020-01-20 17:38:39 +0000

    Develop Usable Update 2 (#7)
    -Adjusted helm to have a more streamline interface to utilise screen space better
    -Fixed theme not loading
    -Added file font-lock+.el - depreciated file after removal of company-box
    -Fixed theme not loading 
    -Updated README to give a brief explanation of the project
    -Improved avy colouring
    -Removed used custom keybinds in favour of default emacs ones
    -Enabled subword-mode to make camel-cased navigation easier
    -Disabled transient-mark-mode to remove screen clutter
    -Changed emacs to start with two windows open
    -Changed indentation to not use tabs
    -Added package async
    -Adjusted aggressive-indent to indent after semi-colon in c modes
    -Reverted back to default company settings
    -Removed package company-box as it had too many issues to maintain
    -Fixed package helm-flycheck being used but never automatically fetched
    -Changed helm-execute-persistent action and complete to have swapped bindings
    -Removed package hl-block mode to improve performance
    -Added package hl-todo
    -Adjusted hl-paren to only highlight closest pair
    -Adjusted hydra-slayer to not immediately close
    -Fixed lsp starting flymake
    -Added package omnisharp-mode
    -Adjusted smooth-scroll to be smoother and improve performance
    -Adjusted undo-tree undo-in-region to improve performance

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,13 +1,5 @@
-
-(add-to-list 'load-path '"~/.emacs.d/elpa/font-lock-plus/")
-(add-to-list 'load-path '"~/.emacs.d/icon-fonts/")
-
-(setenv "PATH" (concat (getenv "PATH") "S:/Program Files/msys64/mingw64/bin"))
-(add-to-list 'exec-path "C:/tools/msys64/usr/bin")
-(add-to-list 'exec-path "C:/tools/msys64/mingw64/bin")
-(add-to-list  'exec-path "S:/Program Files/msys64/mingw64/bin")
-(add-to-list 'exec-path "S:/Program Files/msys64/usr/bin")
-
+;;Extra Load Paths
+(add-to-list 'load-path '"~/.emacs.d/etc/")
 ;;MELPA
 (require 'package)
 (let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
@@ -51,29 +43,22 @@ There are two things you can do about this warning:
   )
 (defun global-keys-setup()
   ;;Sets up personal keybinds after initilization
-  ;;Char Navigation
-  (global-set-key (kbd "C-i") 'previous-line)
-  (global-set-key (kbd "C-j") 'backward-char)
-  (global-set-key (kbd "C-k") 'next-line)
-  (global-set-key (kbd "C-l") 'forward-char)
-  ;;Line Navigation
-  (global-set-key (kbd "C-M-j") 'backward-word)
-  (global-set-key (kbd "C-M-l") 'forward-word)
-  (global-set-key (kbd "<M-up>") 'scroll-up-in-place)
-  (global-set-key (kbd "<M-down>") 'scroll-down-in-place)
+  ;;Emacs Control Bindings
+  (global-set-key (kbd "C-x r") 'revert-buffer)
+  (global-set-key (kbd "M-p") 'scroll-up-in-place)
+  (global-set-key (kbd "M-n") 'scroll-down-in-place)
   ;;sexp Navigation
-  (global-set-key (kbd "C-u") 'sp-backward-sexp)
-  (global-set-key (kbd "C-o") 'sp-forward-sexp)
-  (global-set-key (kbd "C-M-u") 'beginning-of-line)
-  (global-set-key (kbd "C-M-o") 'end-of-line)
   )
+(defun cpp-mode-hook()
+  )
+(add-hook 'c++-mode-hook 'cpp-mode-hook)
 (defun elisp-mode-hook()
   )
 (add-hook 'emacs-lisp-mode 'elisp-mode-hook)
 (defun programming-mode()
   ;;Sets up buffer for programming
   (display-line-numbers-mode)
-  )
+  (electric-indent-mode 0))
 (defvar init-setup-hook nil
   ;;A normal hook that runs at the end of init setup
   )
@@ -91,12 +76,24 @@ There are two things you can do about this warning:
   (menu-bar-mode -1)
   (tool-bar-mode -1)
   (scroll-bar-mode -1)
+  ;; Disable Window Decorations
+  (setq menu-bar-mode nil
+        tool-bar-mode nil
+        scroll-bar-mode nil
+        fring-mode nil
+        )
   (set-frame-parameter nil 'undecorated nil)
-  (fringe-mode 0)
-  ;;Enable built-in modes
-  (global-hl-line-mode)
   ;;Misc Setup
   (global-keys-setup)
+  (delete-other-windows)
+  (split-window-horizontally)
+  (setq inhibit-compacting-font-caches t   ;performance improvement
+        ;; Mode Setting
+        global-subword-mode t              ;easier navigation for camelcasing
+        indent-tabs-mode nil               ;use spaces for indendation
+        transient-mark-mode nil
+        global-hl-line-mode t
+        )
   (add-hook 'prog-mode-hook 'programming-mode)
   (if (not (string= user-login-name "40120333"))
       (run-hooks 'admin-init-setup-hook)
@@ -106,37 +103,55 @@ There are two things you can do about this warning:
 ;;Req Package Setup
 (require 'req-package)
 (setq use-package-always-ensure t)
+(req-package async
+  :config
+  (setq async-bytecomp-package-mode)
+  )
+  (req-package color-theme-sanityinc-tomorrow
+    :config
+    (load-theme 'sanityinc-tomorrow-bright t)
+    )
 (req-package ace-window
   :require ace-window
   :config
   )
 (req-package aggressive-indent
   :hook
-  (prog-mode . aggressive-indent-mode)
-  )
-(req-package all-the-icons
-  :init
-  (setq inhibit-compacting-font-caches t) ;;Improve windows performance
-  )
-(req-package avy
-  :require hydra
+  (emacs-lisp-mode . aggressive-indent-mode)
   :config
-  (global-set-key (kbd "C-f") 'avy-goto-char)
+  (add-to-list 'aggressive-indent-dont-indent-if
+               '(and (derived-mode-p 'c++-mode)
+                     (null (string-match "\\([;{}]\\|\\b\\(if\\|for\\|while\\)\\b\\)"
+                                         (thing-at-point 'line))))
+               )
+  (add-to-list 'aggressive-indent-dont-indent-if
+               '(and (derived-mode-p 'c++-mode)
+                     (string-match "^." (thing-at-point 'line))
+                     )
+               )
+  )
+;; (req-package all-the-icons
+;;   :init
+;;   (setq inhibit-compacting-font-caches t) ;;Improve windows performance
+;;   )
+(req-package avy
+  :config
+  (global-set-key (kbd "C-r") 'avy-goto-char)
   (setq avy-highlight-first t
-	avy-background t
-	)
-  (set-face-attribute 'avy-lead-face nil :background '"firebrick")
-  (set-face-attribute 'avy-lead-face-0 nil :background "navy")
-  (set-face-attribute 'avy-lead-face-1 nil :background "dark olive green")
+        avy-background t
+        )
+  (set-face-attribute 'avy-lead-face nil :background '"firebrick" :foreground "white")
+  (set-face-attribute 'avy-lead-face-0 nil :background "navy" :foreground "white")
+  (set-face-attribute 'avy-lead-face-2 nil :background "dark olive green" :foreground "white")
   )
 (req-package beacon
   :hook
   (init-setup . beacon-mode)
   :config
   (setq beacon-color "gold"
-	beacon-blink-when-point-moves-vertically 1
-	beacon-blink-when-point-moves-horizontally 20
-	)
+        beacon-blink-when-point-moves-vertically 1
+        beacon-blink-when-point-moves-horizontally 20
+        )
   )
 (req-package centaur-tabs
   :hook
@@ -147,80 +162,28 @@ There are two things you can do about this warning:
   :init
   ;;Misc Settings
   (setq centaur-tabs-set-icons t
-  	centaur-tabs-set-modified-marker t
-  	centaur-tabs-set-close-button nil
-  	centaur-tabs-cycle-scope 'tabs
-  	centaur-tabs-style "bar"
-  	centaur-tabs-set-bar t
-	centaur-tabs-bar 'over
-  	centaur-tabs-modified-marker "*"
-  	)
+        centaur-tabs-set-modified-marker t
+        centaur-tabs-set-close-button nil
+        centaur-tabs-cycle-scope 'tabs
+        centaur-tabs-style "bar"
+        centaur-tabs-set-bar t
+        centaur-tabs-bar 'over
+        centaur-tabs-modified-marker "*"
+        )
   :config
   ;;Create Uniform Tabbar Appearance
   (centaur-tabs-headline-match)
   ;;Group tabs by projects
   (centaur-tabs-group-by-projectile-project)
   )
-
-(req-package color-theme-sanityinc-tomorrow
-  :config
-  (load-theme 'color-theme-sanityinc-tomorrow-bright t)
-  )
 (req-package company
   :hook
-  (init-setup . global-company-mode)
-  )
-(req-package company-lsp
-  :require company
+  (prog-mode . company-mode)
   :config
-  (push 'company-lsp company-backends)
-  )
-(req-package company-box
-  :require icons-in-terminal
-  :hook
-  (company-mode . company-box-mode)
-  :config
-  (if (not (string= user-login-name "40120333"))
-      (setq company-box-icons-alist 'company-box-icons-icons-in-terminal
-	    company-box-color-icon t)
-    ;;Icons In Terminal Icons Setup
-    (setq company-box-icons-unknown 'fa_question_circle)
-    ;;Emacs Lisp
-    (setq company-box-icons-elisp
-	  '((fa_tag :face font-lock-function-name-face) ;; Function
-	    (fa_cog :face font-lock-variable-name-face) ;; Variable
-	    (fa_cube :face font-lock-constant-face) ;; Feature
-	    (md_color_lens :face font-lock-doc-face))) ;; Face
-    ;;Language Server Protacol Mode
-    (setq company-box-icons-lsp
-	  '((1 . fa_text_height) ;; Text
-	    (2 . (fa_tags :face font-lock-function-name-face)) ;; Method
-	    (3 . (fa_tag :face font-lock-function-name-face)) ;; Function
-	    (4 . (fa_tag :face font-lock-function-name-face)) ;; Constructor
-	    (5 . (fa_cog :foreground "#FF9800")) ;; Field
-	    (6 . (fa_cog :foreground "#FF9800")) ;; Variable
-	    (7 . (fa_cube :foreground "#7C4DFF")) ;; Class
-	    (8 . (fa_cube :foreground "#7C4DFF")) ;; Interface
-	    (9 . (fa_cube :foreground "#7C4DFF")) ;; Module
-	    (10 . (fa_cog :foreground "#FF9800")) ;; Property
-	    (11 . md_settings_system_daydream) ;; Unit
-	    (12 . (fa_cog :foreground "#FF9800")) ;; Value
-	    (13 . (md_storage :face font-lock-type-face)) ;; Enum
-	    (14 . (md_closed_caption :foreground "#009688")) ;; Keyword
-	    (15 . md_closed_caption) ;; Snippet
-	    (16 . (md_color_lens :face font-lock-doc-face)) ;; Color
-	    (17 . fa_file_text_o) ;; File
-	    (18 . md_refresh) ;; Reference
-	    (19 . fa_folder_open) ;; Folder
-	    (20 . (md_closed_caption :foreground "#009688")) ;; EnumMember
-	    (21 . (fa_square :face font-lock-constant-face)) ;; Constant
-	    (22 . (fa_cube :face font-lock-type-face)) ;; Struct
-	    (23 . fa_calendar) ;; Event
-	    (24 . fa_square_o) ;; Operator
-	    (25 . fa_arrows)) ;; TypeParameter
-	  )
-    )
-  (add-hook 'admin-setup-hook 'company-box-admin-setup)
+  ;; (company-tng-configure-default)
+  (setq company-require-match 'never
+        company-idle-delay 0.1
+        )
   )
 (req-package crux
   :config
@@ -232,7 +195,8 @@ There are two things you can do about this warning:
   )
 (req-package dashboard
   :config
-  (dashboard-setup-startup-hook))
+  (dashboard-setup-startup-hook)
+  )
 (req-package flycheck
   :hook
   (init-setup . global-flycheck-mode)
@@ -268,43 +232,70 @@ There are two things you can do about this warning:
   )
 (req-package helm
   :after hydra
+  :require
+  helm-projectile
+  helm-flycheck
+  helm-swoop
+  :init
+  ;; Completley hide helm header
+  (fset 'helm-display-mode-line #'ignore)
+  (defadvice helm-display-mode-line (after undisplay-header activate)
+    (setq header-line-format nil)
+    )
+  (add-hook 'helm-after-initialize-hook
+            (defun hide-mode-line-in-helm-buffer ()
+              "Hide mode line in `helm-buffer'."
+              (with-helm-buffer
+                (setq-local mode-line-format nil)
+                )
+              )
+            )
   :config
   (require 'helm-config)
   (helm-mode)
   ;;Helm minibuffer config
   (setq helm-autoresize-mode t
-	helm-display-header-line nil
-	helm-header-line-space-before-prompt nil
-	helm-autoresize-max-height 40   ;Always takes up half the screen
-	helm-autoresize-min-height 40
-	helm-split-window-in-side-p t   ;Shows helm window in current buffer
-	)
+        helm-display-header-line nil
+        helm-header-line-space-before-prompt nil
+        helm-autoresize-max-height 40   ;Always takes up half the screen
+        helm-autoresize-min-height 40
+        helm-split-window-in-side-p t   ;Shows helm window in current buffer
+        helm-mode-line-string nil
+        )
+  :config
+  (require 'helm-config)
+ ;;;Helm minibuffer config
+  ;; Don't use helm's own displaying mode line function
   (set-face-attribute 'helm-source-header nil
-		      :height 1.1
-		      :foreground "dark cyan"
-		      )
+                      :height 1.1
+                      :foreground "dark cyan"
+                      )
   ;; (set-face-attribute 'helm-eob-line nil :height 0.1)
   ;; (set-face-attribute 'helm-helper nil :height 0.1))
   ;; (defvar helm-source-header-default-box (face-attribute 'helm-source-header :box))
   ;; (defun helm-toggle-header-line ()
   ;;   (if (> (length helm-sources) 1)
-  ;; 	(set-face-attribute 'helm-source-header
-  ;; 			    nil
-  ;; 			    :foreground helm-source-header-default-foreground
-  ;; 			    :background helm-source-header-default-background
-  ;; 			    :box helm-source-header-default-box
-  ;; 			    :height 1.0)
+  ;;    (set-face-attribute 'helm-source-header
+  ;;                        nil
+  ;;                        :foreground helm-source-header-default-foreground
+  ;;                        :background helm-source-header-default-background
+  ;;                        :box helm-source-header-default-box
+  ;;                        :height 1.0)
   ;;     (set-face-attribute 'helm-source-header
-  ;; 			  nil
-  ;; 			  :foreground (face-attribute 'helm-selection :background)
-  ;; 			  :background (face-attribute 'helm-selection :background)
-  ;; 			  :box nil
-  ;; 			  :height 0.1)))
+  ;;                      nil
+  ;;                      :foreground (face-attribute 'helm-selection :background)
+  ;;                      :background (face-attribute 'helm-selection :background)
+  ;;                      :box nil
+  ;;                      :height 0.1)))
   ;; (add-hook 'helm-before-initialize-hook 'helm-toggle-header-line)
-					;(add-hook 'helm-before-initialize-hook 'helm-toggle-header-line)
-  ;;Global Keys
+                                        ;(add-hook 'helm-before-initialize-hook 'helm-toggle-header-line)
+  ;;Keybinds
   (global-set-key (kbd "M-x") 'helm-M-x)
   (global-set-key (kbd "C-x C-f") 'helm-find-files)
+  ;;Swap Action and Completion Buttons
+  (define-key helm-map (kbd "TAB") #'helm-execute-persistent-action)
+  (define-key helm-map (kbd "<tab>") #'helm-execute-persistent-action)
+  (define-key helm-map (kbd "C-z") #'helm-select-action)
   ;;Query
   (defhydra hydra-query (global-map "C-q" :color blue)
     "query for"
@@ -314,62 +305,45 @@ There are two things you can do about this warning:
     ("f" helm-flycheck "flycheck")
     )
   )
-(req-package helm-projectile
-  :require helm
-  :config
-  )
-(req-package helm-swoop
-  :require helm
-  :config
-  )
-(req-package hl-block-mode
-  :hook
-  (prog-mode . hl-block-mode)
-  )
 (req-package highlight-parentheses
   :hook
   (init-setup . global-highlight-parentheses-mode)
   :config
-  (setq hl-paren-background-colors '("white"
-				     "dark orange"
-				     "deep pink"
-				     "chartreuse"
-				     "deep sky blue"
-				     "yellow"
-				     "orchid"
-				     "spring green"
-				     "sienna1"
-				     )
-	hl-paren-colors '("black")
-	hl-paren-highlight-adjacent t
-	)
+  (setq hl-paren-background-colors '("gray"
+                                     )
+        hl-paren-colors '("black")
+        hl-paren-highlight-adjacent nil
+        )
+  )
+(req-package hl-todo
+  :config
+  (global-hl-todo-mode
+   )
   )
 (req-package hydra
   :config
   (defhydra hydra-slayer (global-map "C-s" :color blue)
     "kill shortcuts"
-    ("x" slay-function "function(x)")
-    ("l" kill-whole-line "whole line")
+    ("x" slay-function "function(x)" :color red)
+    ("l" kill-whole-line "whole line" :color red)
     ("b" kill-whole-buffer "whole buffer")
     )
-					;(defhydra hydra-grab (:color)
-					;  )
-					;(global-set-key (kbd "C-g") (hydra-grab/body))
-  )
-(req-package icons-in-terminal
-  :ensure nil
-  )
-(req-package irony
-  :config
+                                        ;(defhydra hydra-grab (:color)
+                                        ;  )
+                                        ;(global-set-key (kbd "C-g") (hydra-grab/body))
   )
 (req-package lsp-mode
+  :after company
+  :require company-lsp
   :hook
   (c++-mode . lsp)
   :config
-  (setq lsp-enable-snippet nil)
+  (setq lsp-enable-snippet nil
+        lsp-prefer-flymake nil)
+  (push 'company-lsp company-backends)
   )
 (req-package lsp-ui
-  :require
+  :after
   lsp-mode
   flycheck
   :config
@@ -378,10 +352,6 @@ There are two things you can do about this warning:
   :require magit
   :config
   )
-;; (req-package powerline
-;;   :config
-;;   (add-hook 'after-init-hook 'powerline-default-theme)
-;;   )
 (req-package multiple-cursors
   :require hydra
   :config
@@ -393,7 +363,10 @@ There are two things you can do about this warning:
   ;;   ("r" mc/mark-all-in-region "mark in region")
   ;;   )
   )
-
+(req-package omnisharp
+  ;; :hook
+  ;; (csharp-mode . omnisharp-mode)
+  )
 (req-package projectile
   :hook
   (init-setup . projectile-mode)
@@ -426,8 +399,9 @@ There are two things you can do about this warning:
   (set-face-attribute 'rainbow-delimiters-depth-9-face nil :foreground "sienna1")
   )
 (req-package rainbow-mode
-  :hook
-  (prog-mode . rainbow-mode))
+:hook
+(prog-mode . rainbow-mode)
+)
 (req-package restart-emacs
   :config
   (global-set-key (kbd "C-x C-a") 'restart-emacs)
@@ -449,23 +423,23 @@ There are two things you can do about this warning:
   :hook
   (init-setup . smooth-scrolling-mode)
   :config
-  (setq scroll-conservatively 2)
+  (setq scroll-conservatively nil)
   )
 (req-package sublimity
   :hook
   (init-setup . sublimity-mode)
   :config
   (require 'sublimity-scroll)
-  (require 'sublimity-attractive)
+  ;; (require 'sublimity-attractive)
   ;;(require 'sublimity-map) ;; experimental
 
   (setq sublimity-scroll-weight 15
-	sublimity-scroll-drift-length 0
-	;;	sublimity-map-size 10
-	;;      sublimity-map-fraction 0.3
-	;;	sublimity-map-text-scale -7
-	;;	sublimity-map-set-delay 1
-	)
+        sublimity-scroll-drift-length 0
+        ;;      sublimity-map-size 10
+        ;;      sublimity-map-fraction 0.3
+        ;;      sublimity-map-text-scale -7
+        ;;      sublimity-map-set-delay 1
+        )
   )
 (req-package treemacs
   )
@@ -474,13 +448,16 @@ There are two things you can do about this warning:
   (global-undo-tree-mode)
   (global-set-key (kbd "C-z") 'undo-tree-undo)
   (global-set-key (kbd "C-S-z") 'undo-tree-redo)
+  (setq undo-tree-enable-undo-in-region nil)
   )
 (req-package volatile-highlights
-  :config
-  (volatile-highlights-mode))(req-package ws-butler
-  :config
-  (ws-butler-global-mode)
-  )
+:config
+(volatile-highlights-mode)
+)
+(req-package ws-butler
+:config
+(ws-butler-global-mode)
+)
 (req-package zoom
   )
 ;;Solve Dependencies and Load in Correct Order
@@ -488,15 +465,7 @@ There are two things you can do about this warning:
 (req-package-finish)
 ;;Automatic Custom Variables
 (custom-set-variables)
-'(custom-enabled-themes)
-'(custom-safe-themes)
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(custom-set-faces)
 
 (init-setup)
 (provide 'init)
